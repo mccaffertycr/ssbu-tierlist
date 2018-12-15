80fe2c15772fc217676c8ca535a502ccc9bf45d8
mccaffertycr <mccaffertycr@gmail.com>
2018-12-15 01:55:56 -0500

    controller+routes

--- server/api/routes/tierlist.js
+++ server/api/routes/tierlist.js
@@ -1,6 +1,18 @@
 const router = require('express').Router();
 const tierlistController = require('../../controllers/tierlist.controller');
 
-router.route('/').get(tierlistController.findAllTierlists);
+router
+  .route('/')
+  .get(tierlistController.findAllTierlists)
+  .put(tierlistController.createTierlist);
+
+router.route('/new').get(tierlistController.findNewTierlists);
+
+router.route('/top').get(tierlistController.findTopTierlists);
+
+router
+  .route('/:id')
+  .get(tierlistController.findTierlistById)
+  .put(tierlistController.upvoteTierlist);
 
 module.exports = router;
--- server/controllers/tierlist.controller.js
+++ server/controllers/tierlist.controller.js
@@ -1,10 +1,52 @@
 const tierlist = require('../database/models/tierlist');
 
 module.exports = {
+  findTierlistById: (req, res) => {
+    const { id } = req.body;
+    tierlist
+      .findById(id)
+      .then(dbTierlist => res.json(dbTierlist))
+      .catch(err => res.status(422).json(err));
+  },
   findAllTierlists: (req, res) => {
+    const { limit, offset } = req.body;
     tierlist
       .find({})
+      .limit(limit)
+      .skip(offset)
       .then(dbTierlists => res.json(dbTierlists))
+      .catch(err => res.status(422).json(err));
+  },
+  findNewTierlists: (req, res) => {
+    const { limit, offset } = req.body;
+    tierlist
+      .find({})
+      .limit(limit)
+      .skip(offset)
+      .sort({ createdAt: -1 })
+      .then(dbTierlists => res.json(dbTierlists))
+      .catch(err => res.status(422).json(err));
+  },
+  findTopTierlists: (req, res) => {
+    const { limit, offset } = req.body;
+    tierlist
+      .find({})
+      .limit(limit)
+      .skip(offset)
+      .then(dbTierlists => res.json(dbTierlists))
+      .catch(err => res.status(422).json(err));
+  },
+  createTierlist: (req, res) => {
+    tierlist
+      .create(req.body)
+      .then(dbTierlist => console.log(dbTierlist))
+      .catch(err => res.status(422).json(err));
+  },
+  upvoteTierlist: (req, res) => {
+    const { id } = req.body;
+    tierlist
+      .findByIdAndUpdate(id, { $inc: { upvotes: 1 } })
+      .then(dbTierlist => console.log(dbTierlist))
       .catch(err => res.status(422).json(err));
   },
 };
